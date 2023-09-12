976768c8dd07338dfe409691fc61962c5d88fd48
Bart Wolters <wolt0017@hz.nl>
2023-09-12 09:27:27 +0200

    tailwind commit

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,16 @@
+<script>
+    	let bgColor = 'primary'
+</script>
+
+<div class="box">
+        <label for="bg-color-box">{bgColor}</label>
+        <input id="bg-color-box" type="color" bind:value={bgColor} />
+</div>
+
+<style>
+	.box {
+		border: 1px solid #aaa;
+		padding: 1em;
+		margin: 20px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Example from '$lib/components/Example.svelte';
+    import Box from '$lib/components/Box.svelte';
 
 </script>
 
 <div class="container w-full mx-auto">
     <h1>Welcome to Sveltekit</h1>
     <Example />
+    <Box />
 </div>
 
 <style>
